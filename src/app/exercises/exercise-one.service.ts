4d2d3dc4191d02d42e4f416e133958afa44fee47
nilsjondral <nils.jondral@telenet.be>
2018-04-07 11:25:32 +0200

    removed solutions

--- src/app/exercises/exercise-one.service.ts
+++ src/app/exercises/exercise-one.service.ts
@@ -15,27 +15,27 @@ export class ExerciseOneService {
   filterOutValues(): Observable<number> {
     const numbers$: Observable<number> = this.dataService.getStreamOfNumbers();
 
-    return numbers$.pipe(filter(x => x % 2 === 0));
+    return numbers$;
   }
 
   // TODO: Multiply all the number events by two
   multiplyAllValuesByTwo() {
     const numbers$: Observable<number> = this.dataService.getStreamOfNumbers();
 
-    return numbers$.pipe(map(x => x * 2));
+    return numbers$;
   }
 
   // TODO: Every event is an array of numbers. Modify the event so the array only contains even numbers.
   filterOutValuesOfArray(): Observable<Array<number>> {
     const array$: Observable<Array<number>> = this.dataService.getStreamOfArrayWithNumbers();
 
-    return array$.pipe(map(arr => arr.filter(x => x % 2 === 0)));
+    return array$;
   }
 
   // TODO: Every event is an array of numbers. Modify the event so every number in the array is multiplied by two.
   multiplyAllValuesByTwoOfArray(): Observable<Array<number>> {
     const array$: Observable<Array<number>> = this.dataService.getStreamOfArrayWithNumbers();
 
-    return array$.pipe(map(arr => arr.map(x => x * 2)));
+    return array$;
   }
 }
