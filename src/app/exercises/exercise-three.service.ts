a1a6be5e439a0106c350dc9925f4f9a477040da1
nilsjondral <nils.jondral@telenet.be>
2018-04-15 15:33:48 +0200

    removed solutions exercise three

--- src/app/exercises/exercise-three.service.ts
+++ src/app/exercises/exercise-three.service.ts
@@ -2,16 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { DataService } from '../services/data.service';
 import { Scheduler } from 'rxjs/Scheduler';
-import { debounceTime,
-  distinctUntilChanged,
-  filter,
-  switchMap,
-  merge,
-  startWith,
-  combineLatest,
-  mergeMap,
-  concatMap
-} from 'rxjs/operators';
 
 @Injectable()
 export class ExerciseThreeService {
@@ -24,13 +14,7 @@ export class ExerciseThreeService {
   // TODO: Avoid having unnecessary backend calls
   // TODO: (use the dataService.getBackendData for the backend call)
   autoComplete(searchTerm$: Observable<string>) {
-    return searchTerm$
-      .pipe(
-        debounceTime(20, this.scheduler),
-        distinctUntilChanged(),
-        filter(x => x.length > 1),
-        switchMap(y => this.dataService.getBackendData(y))
-      );
+    return searchTerm$;
   }
 
   // TODO: Every time the user clicks OR swipes down on his mobile screen the data needs to be reloaded.
@@ -38,29 +22,19 @@ export class ExerciseThreeService {
   // TODO: Make sure that a backend call is performed on startup as well
   // TODO: (use the dataService.getBackendData without parameters for the backend call)
   refreshTheData(click$: Observable<string>, swipe$: Observable<string>) {
-    return click$
-      .pipe(
-        merge(swipe$),
-        startWith(''),
-        switchMap(x => this.dataService.getBackendData(x))
-      );
+    return click$;
   }
 
   // TODO: Every time the page or searchTerm changes a backend call must be performed.
   // TODO: Avoid multiple backend calls
   // TODO: (use the dataService.getBackendData, pass the searchTerm and page as parameters)
   refreshTheData2(page$: Observable<number>, searchTerm$: Observable<string>) {
-    return searchTerm$
-      .pipe(
-        combineLatest(page$),
-        mergeMap(x => this.dataService.getBackendData(x[0], x[1]))
-      );
+    return searchTerm$;
   }
 
   // TODO: For every event in the pictures$, an image should be uploaded
   // TODO: (use the dataService.uploadPicture for the backend call)
   uploadPictures(pictures$: Observable<string>) {
-    return pictures$
-      .pipe(concatMap(x => this.dataService.uploadPicture(x)));
+    return pictures$;
   }
 }
